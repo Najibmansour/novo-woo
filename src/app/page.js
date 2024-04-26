dae031da45abfef42c2a4a4120573c79d4c65a55
Najibmansouur <najibmansour2005@gmail.com>
2024-04-27 02:29:16 +0300

    ADD: adding fetching products functions to the api

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 import { getAllCategories, getAllProducts } from "@/utils/api/api";
 
 export default async function Home() {
-  const products = await getAllProducts();
+  const products = await getAllProducts(6, 5, "price", true);
   const categories = await getAllCategories();
 
-  // console.log(categories);
+  console.log("__________________________________________________");
 
+  // products.forEach((prod) => {
+  //   console.log(prod.id, "::", prod.price, "::", prod.name);
+  // });
+
+  console.log(categories);
   return <main></main>;
 }
--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -1,9 +1,19 @@
 "use server";
 import { client } from "../client/woocomerce-client";
 
-export async function getAllProducts() {
+export async function getAllProducts(
+  page = 1,
+  per_page = 60,
+  orderby = "price",
+  asc = false
+) {
   try {
-    const response = await client.get("products");
+    const response = await client.get("products", {
+      orderby: orderby,
+      order: asc ? "asc" : "desc",
+      page: page,
+      per_page: per_page,
+    });
     return response.data;
   } catch (error) {
     console.warn(error);
@@ -18,3 +28,27 @@ export async function getAllCategories() {
     console.warn(error);
   }
 }
+
+export async function getProductsByCategory(category_id = 0) {
+  try {
+    const response = await client.get(`products?category=${category_id}`);
+    return response.data;
+  } catch (error) {
+    console.warn(error);
+  }
+}
+
+export async function getProductsFromArrayOfCategory(category_ids = []) {
+  try {
+    let productsByCategory = [];
+    category_ids = category_ids.forEach((id) => {
+      productsByCategory.push(getProductsByCategory(id));
+    });
+    return response.data;
+  } catch (error) {
+    console.warn(error);
+  }
+}
+
+// TODO: fn that returns all products from category :: param=category_id
+// TODO: fn that return all products in specific counntry :: REQUIRES PLUGGIN "multi-currency"
