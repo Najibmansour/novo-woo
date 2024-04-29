6ef77f9e71d9653419b7d55ec7b2ebc420251588
Najibmansouur <najibmansour2005@gmail.com>
2024-04-30 01:20:16 +0300

    ADD: signup page

---
+++ src/app/(Auth)/signup/page.jsx
@@ -0,0 +1,12 @@
+import Signup from "@/components/Auth/signup";
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <Signup />
+    </main>
+  );
+};
+
+export default page;
