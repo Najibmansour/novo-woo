de6ea48c8d3e7d63b6d4d81efdd9feaa0bc38d80
Najibmansouur <najibmansour2005@gmail.com>
2024-04-29 21:20:04 +0300

    Before NEXTSUI installation

--- src/app/page.js
+++ src/app/page.js
@@ -7,15 +7,8 @@ import {
 export default async function Home() {
   console.log("__________________________________________________");
 
-  // const products = await getAllProducts(6, 5, "price", true);
   const categories = await getAllCategories();
   const prodsByCat = await getProductsFromArrayOfCategory(["15", "18", "21"]);
 
-  // prodsByCat.forEach((prod) => {
-  //   console.log(prod);
-  // });
-
-  console.log(categories);
-  console.table(prodsByCat);
   return <main></main>;
 }
