e5a2800770e04b4d2544b087fd6510169c5f204f
Najibmansouur <najibmansour2005@gmail.com>
2024-04-30 01:21:05 +0300

    ADD: logo

---
+++ src/components/Auth/signup.jsx
@@ -0,0 +1,47 @@
+import {
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Card,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+import LOGOIMG from "../../../public/photos/logo.jpg";
+
+export default function Signup() {
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <Image src={LOGOIMG} width={400} height={200} alt="logo-img" />
+        <CardTitle>Create your account</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" placeholder="Enter an email" type="email" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                placeholder="Enter a password"
+                type="password"
+              />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Sign Up</Button>
+      </CardFooter>
+    </Card>
+  );
+}
