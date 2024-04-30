0fc81d9dce43044cc6020b4b30a495b92636be61
Najibmansouur <najibmansour2005@gmail.com>
2024-05-01 02:07:39 +0300

    ADD: error label component to display errors for forms

--- src/components/ui/errorlabel.jsx
+++ src/components/ui/errorlabel.jsx
@@ -7,7 +7,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const labelVariants = cva(
-  "text-sm text-red-700 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+  "text-xs text-red-700 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 );
 
 const ErrorLabel = React.forwardRef(({ className, ...props }, ref) => (
